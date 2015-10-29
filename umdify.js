be690ec35341b218977afd029d5224ac0b5b3470
Mohsen Azimi <me@azimi.me>
2015-10-29 11:15:58 -0700

    Fix browser global export

--- umdify.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const umdify = require('libumd');
-
-const src = fs.readFileSync('./dist/index.js').toString();
-
-fs.writeFileSync('./dist/browser.js', umdify(src, {globalAlias: 'JSONToJSONSchema'}));
